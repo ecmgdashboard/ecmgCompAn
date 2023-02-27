a53b9336691e7307225d8141a6fa4444eb67f341
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2023-02-26 19:38:43 -0500

    tanmay trial push

--- 🥇_Competitive_Analytics.py
+++ 🥇_Competitive_Analytics.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-<<<<<<< HEAD
 st.markdown("# Competitive Analytics lmao- pushed by tanmay")
-=======
 st.markdown("# Competitive Analytics")
 st.markdown("Test")
->>>>>>> origin/main
